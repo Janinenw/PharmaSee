c945fa90b15cff4470f0d7f7eaa053c348770537
Janinenw <janine.wexler@gmail.com>
2023-03-13 17:21:58 -0400

    database recreated to have proper name, data seeded via seed.js

--- controllers/pharmacies.js
+++ controllers/pharmacies.js
@@ -1,8 +1,9 @@
 
+const { application } = require('express');
 const express = require('express')
 const router = express.Router()
 const Pharmacy = require('../models/pharmacies.js')
-// const mongoose = require('mongoose');
+const seedData = require('../models/seed.js')
 
 
 //Index
@@ -30,45 +31,97 @@ router.get('/new', (req, res) => {
 
 
 
+// router.get('/seed', (req, res) => {
+//     Pharmacy.create([
+//         {
+//             name: 'Rite Aid',
+//             town: 'Fairfield',
+//             address: '1619 Post Road',
+//             phoneNumber: '(203) 259-2353',
+//             dateUpdated: '2023-03-07',
+//             inStock: true
+//         },
+//         {
+//             name: 'Walgreens',
+//             town: 'Westport',
+//             address: '880 Post Road East',
+//             phoneNumber: '(203) 226-8452',
+//             dateUpdated: '2023-03-03',
+//             inStock: false
+//         },
+//         {
+//             name: 'CVS',
+//             town: 'Fairfield',
+//             address: '700 Post Road',
+//             phoneNumber: '(203) 255-1089',
+//             dateUpdated: '2023-03-03',
+//             inStock: false
+//         }
+//     ], (err, createdPharmacies) => {
+//         if (err) {
+//             console.log(err.message);
+//         } else {
+//             console.log("data seeded");
+//             res.redirect("/pharmacy");
+//         }
+//     });
+// });
+
+// router.get('/seed', (req, res) => {
+//     Pharmacy.create([
+//         {
+//             name: 'Rite Aid',
+//             town: 'Fairfield',
+//             address: '1619 Post Road',
+//             phoneNumber: '(203) 259-2353',
+//             dateUpdated: '2023-03-07',
+//             inStock: true
+//         },
+//         {
+//             name: 'Walgreens',
+//             town: 'Westport',
+//             address: '880 Post Road East',
+//             phoneNumber: '(203) 226-8452',
+//             dateUpdated: '2023-03-03',
+//             inStock: false
+//         },
+//         {
+//             name: 'CVS',
+//             town: 'Fairfield',
+//             address: '700 Post Road',
+//             phoneNumber: '(203) 255-1089',
+//             dateUpdated: '2023-03-03',
+//             inStock: false
+//         }
+//     ], (err, createdPharmacies) => {
+//         if (err) {
+//             console.log(err.message);
+//         } else {
+//             console.log("data seeded");
+//             res.redirect("/pharmacy");
+//         }
+//     });
+// });
+
+
+// router.get('/seed', (req, res) =>{
+// 	pharmacy.create([
+
+// 	], (err, data) => {
+// 		res.redirect('/pharmacy')
+// 	})
+// })
+
 router.get('/seed', (req, res) => {
-    Pharmacy.create([
-        {
-            name: 'Rite Aid',
-            town: 'Fairfield',
-            address: '1619 Post Road',
-            phoneNumber: '(203) 259-2353',
-            dateUpdated: '2023-03-07',
-            inStock: true
-        },
-        {
-            name: 'Walgreens',
-            town: 'Westport',
-            address: '880 Post Road East',
-            phoneNumber: '(203) 226-8452',
-            dateUpdated: '2023-03-03',
-            inStock: false
-        },
-        {
-            name: 'CVS',
-            town: 'Fairfield',
-            address: '700 Post Road',
-            phoneNumber: '(203) 255-1089',
-            dateUpdated: '2023-03-03',
-            inStock: false
-        }
-    ], (err, createdPharmacies) => {
+    Pharmacy.create(seedData, (err, data) => {
         if (err) {
             console.log(err.message);
         } else {
-            console.log("data seeded");
-            res.redirect("/pharmacy");
+            console.log(seedData);
         }
+        res.redirect('/pharmacy');
     });
 });
-
-
-
-
 //delete
 
 router.delete('/:id', (req, res) => {
@@ -82,6 +135,7 @@ router.delete('/:id', (req, res) => {
         })
     }
 })
+
 
 //update
 
